d71475fc9d34cfb34b0254cb691646178495dca0
Isaque correa <isq2006@gmail.com>
2023-05-08 15:48:30 -0300

    header mobile

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
-console.log("eu sou o mestre")
+function menuShow() {
+  let menuMobile = document.querySelector(".header__nav");
+  if (menuMobile.classList.contains("open")) {
+    menuMobile.classList.remove("open");
+  } else {
+    menuMobile.classList.add("open");
+  }
+  let menuActive = document.querySelector(".header__burger");
+  if (menuActive.classList.contains("active")) {
+    menuActive.classList.remove("active");
+  } else {
+    menuActive.classList.add("active");
+  }
+}
