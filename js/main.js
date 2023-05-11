852a4625cd1ce791aa9f891b226c8ed924794414
Isaque correa <isq2006@gmail.com>
2023-05-11 16:09:55 -0300

    footer mobile

--- js/main.js
+++ js/main.js
@@ -21,35 +21,61 @@ function dropdownShow() {
     dropdown.classList.add("hide");
   }
 }
+
 function dropdownShow2() {
-    let dropdown = document.querySelector(".dropdown__date");
-    if (dropdown.classList.contains("hide")) {
-      dropdown.classList.remove("hide");
-    } else {
-      dropdown.classList.add("hide");
-    }
+  let dropdown = document.querySelector(".dropdown__date");
+  if (dropdown.classList.contains("hide")) {
+    dropdown.classList.remove("hide");
+  } else {
+    dropdown.classList.add("hide");
   }
-  function dropdownShow3() {
-    let dropdown = document.querySelector(".dropdown__date3");
-    if (dropdown.classList.contains("hide")) {
-      dropdown.classList.remove("hide");
-    } else {
-      dropdown.classList.add("hide");
-    }
+}
+
+function dropdownShow3() {
+  let dropdown = document.querySelector(".dropdown__date3");
+  if (dropdown.classList.contains("hide")) {
+    dropdown.classList.remove("hide");
+  } else {
+    dropdown.classList.add("hide");
   }
-  function dropdownShow4() {
-    let dropdown = document.querySelector(".dropdown__select");
-    if (dropdown.classList.contains("hide")) {
-      dropdown.classList.remove("hide");
-    } else {
-      dropdown.classList.add("hide");
-    }
+}
+function dropdownShow4() {
+  let dropdown = document.querySelector(".dropdown__select");
+  if (dropdown.classList.contains("hide")) {
+    dropdown.classList.remove("hide");
+  } else {
+    dropdown.classList.add("hide");
   }
-  function dropdownShow5() {
-    let dropdown = document.querySelector(".dropdown__searchInput");
-    if (dropdown.classList.contains("searchShow")) {
-      dropdown.classList.remove("searchShow");
-    } else {
-      dropdown.classList.add("searchShow");
-    }
+}
+function dropdownShow5() {
+  let dropdown = document.querySelector(".dropdown__searchInput");
+  if (dropdown.classList.contains("searchShow")) {
+    dropdown.classList.remove("searchShow");
+  } else {
+    dropdown.classList.add("searchShow");
   }
+}
+function footerShow() {
+  let dropdown = document.querySelector(".footer__homeLink");
+  if (dropdown.classList.contains("homeShow")) {
+    dropdown.classList.remove("homeShow");
+  } else {
+    dropdown.classList.add("homeShow");
+  }
+}
+function footerShow1() {
+  let dropdown = document.querySelector(".footer__helpLink");
+  if (dropdown.classList.contains("homeShow")) {
+    dropdown.classList.remove("homeShow");
+  } else {
+    dropdown.classList.add("homeShow");
+  }
+}
+function footerShow2() {
+  let dropdown = document.querySelector(".footer__getAppLink");
+  if (dropdown.classList.contains("homeShow")) {
+    dropdown.classList.remove("homeShow");
+  } else {
+    dropdown.classList.add("homeShow");
+  }
+}
